32921b7a2b7ec44f7163c82059d8f694d2ae1c92
Tuan Phung <tuan@phung.cz>
2018-01-06 13:39:54 +0100

    fix

--- index.js
+++ index.js
@@ -14,45 +14,47 @@ var oldPageString;
 
 const makeMagic = () => {
 
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  // const date = new Date();
+  // const year = date.getFullYear();
+  // const month = date.getMonth() + 1;
+  // const day = date.getDate();
+  // const hour = date.getHours();
+  // const minute = date.getMinutes();
   
 
-  const currentTime = `${year}-${month}-${day}-${hour}:${minute}`;
+  // const currentTime = `${year}-${month}-${day}-${hour}:${minute}`;
 
   if (number === 0) {
     request(
-      "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm",
+      // "http://www.phung.cz",
+      'http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm', 
       function(err, resp, html) {
         let $page = cheerio.load(html);
         $page(".view-id-media_video").remove();
         oldPageString = $page("body").html();
-        fs.writeFile(`./file.html`, oldPageString, err => {
-          if (err) return;
-          console.log("default file has been created");
-          number++;
-        });
+        // fs.writeFile(`./file.html`, oldPageString, err => {
+        //   if (err) return;
+        //   console.log("default file has been created");
+   
+        // });
+        number++;
       }
     );
   } else {
     request(
-      "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm",
+      // "http://www.phung.cz",
+      'http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm',
       function(err, resp, dom) {
         let $page = cheerio.load(dom);
         $page(".view-id-media_video").remove();
         let domBody = $page("body").html();
+
         if (oldPageString !== domBody && number === 1) {
-          // console.log(oldPageString)
-          console.log(oldPageString);
-          fs.writeFile(`./file-${currentTime}.html`, domBody, err => {
-            if (err) return;
-            console.log("new file has been created");
-            return;
-          });
+          // fs.writeFile(`./file-${currentTime}.html`, domBody, err => {
+          //   if (err) return;
+          //   console.log("new file has been created");
+          //   return;
+          // });
           sgMail.setApiKey(
             "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"
           );
@@ -61,14 +63,20 @@ const makeMagic = () => {
             from: "ahoj@jaksemaas.cz",
             subject: "TKY new",
             text: `${domBody}`,
+          };
+          const msgHtml = {
+            to: "thenewworld@seznam.cz",
+            from: "ahoj@jaksemaas.cz",
+            subject: "TKY new html",
             html: `${domBody}`,
           };
-          console.log("not the same");
-
-         return  sgMail.send(msg);
+          sgMail.send(msgHtml);
+          sgMail.send(msg);
+          console.log('email send')
+         return number++
           //
         }
-        console.log("STIL THE SSAME");
+        console.log('run out - web is the same')
       }
     );
   }
@@ -79,17 +87,22 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => {
-    var number = 0;
-    var oldPageString;
-    console.log(makeMagic);
-    return setInterval(makeMagic, 60000);
+    return setInterval(makeMagic, 500);
 
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}!!!!!!!!!!!!`));
+  .get("/alive", (req, res) => {
+    // I'm I alive endpoint
+    return res.render('pages/index')
+
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}!`));
 
 
 
-
+// this IFFEE is only for heroku 
 (function initialCall() {
   http.get("http://nameless-plains-69824.herokuapp.com");
 })();
+
+// to run the code localy `yarn start` and go to localhost:5000
+
